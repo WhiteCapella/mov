4bbd175732f92afaf90928fbebb31461265db126
WhiteCapella <whitecapella.official@gmail.com>
2024-08-01 14:21:48 +0900

    param add

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -9,8 +9,6 @@ def apply_type2df(load_dt, path):
     #TODO
     #저장한 parquet 파일들을 변형시키기
     df = pd.read_parquet(f'{path}/load_dt={load_dt}')
-    df['rnum'] = pd.to_numeric(df['rnum'])
-    df['rank'] = pd.to_numeric(df['rank'])
     num_cols = ['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt', 'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten', 'salesChange', 'audiInten', 'audiChange']
     
     for c in num_cols:
@@ -18,23 +16,21 @@ def apply_type2df(load_dt, path):
 
     return df
 
-def save2df(load_dt):
-    df = list2df(load_dt)
+def save2df(load_dt, url_param={}):
+    df = list2df(load_dt, url_param)
     # df에 load_dt column 추가 (조회 일자 : YYYYMMDD)
     # 아래 파일 저장시 load_dt 기준으로 파티셔닝
     df['load_dt']=load_dt
-    print(df.head(5))
-    df.to_parquet('~/tmp/test_parquet', partition_cols=['load_dt'])
     return df
 
 
-def list2df(load_dt) -> list:
-    l = req2list(load_dt)
+def list2df(load_dt, url_param={}):
+    l = req2list(load_dt, url_param)
     df = pd.DataFrame(l)
     return df
 
-def req2list(load_dt) -> list:
-    _, data = req(load_dt)
+def req2list(load_dt, url_param={}):
+    _, data = req(load_dt, url_param)
     l = data['boxOfficeResult']['dailyBoxOfficeList']
     return l
 
@@ -42,19 +38,22 @@ def get_key():
     key = os.getenv('MOVIE_API_KEY')
     return key
 
-def req(load_dt):
+def req(load_dt, url_param={}):
     #url = gen_url('20240720')
-    url = gen_url(load_dt)
+    url = gen_url(load_dt, url_param)
     r = requests.get(url)
     code = r.status_code
     data = r.json()
-    print(data)
     return code, data
+
+
 
 
 def gen_url(dt):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
     url = f"{base_url}?key={key}&targetDt={dt}"
+    for k, v in url_param.items():
+        url = url + f"&{k}={v}"
 
     return url
